de7ecf57b9186adfeb85c500b5e976c9fb8818ef
SeppoVolsk <serjanya415@gmail.com>
2023-03-10 19:21:52 +0300

    Fix snackbar duration

--- lib/common/ui/app_components/app_snack_bar.dart
+++ lib/common/ui/app_components/app_snack_bar.dart
@@ -15,7 +15,7 @@ abstract class AppSnackBar {
 
   static void showSnackBarWithError(BuildContext context, ErrorEntity error) {
     ScaffoldMessenger.maybeOf(context)?.showSnackBar(SnackBar(
-        duration: const Duration(seconds: 50),
+        duration: const Duration(seconds: 5),
         content: SingleChildScrollView(
           child: Text(
             "Error: ${error.message}",
